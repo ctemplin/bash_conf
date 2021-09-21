e6418714e25675ad48b595fdedb50399366eb50d
Templin, Chris <ctemplin@gmail.com>
2021-09-21 10:07:24 -0400

    Optional workspace num var. Handling no matching workspace.json file. Dynamic config directory.

--- .bashrc.d/i3-current-workspace.sh
+++ .bashrc.d/i3-current-workspace.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+i3-config-dir ()
+{
+    dirname `i3-msg -t get_version | jq -r .loaded_config_file_name`
+}
+
 i3-ws-num ()
 {
     i3-msg -t get_workspaces | jq '.[] | select(.focused)| .num'
@@ -7,12 +12,30 @@ i3-ws-num ()
 
 i3-wsj-file ()
 {
-    wsfile=~/github_repos/i3_conf/.i3/ws/workspace-`i3-ws-num`-*
-    echo $wsfile
+    local WSNUM
+    if [ -z "$1" ]; then WSNUM=$(i3-ws-num); else WSNUM=$1; fi
+    wsfile=$(i3-config-dir)/ws/workspace-${WSNUM}-*
+    if [ -f $wsfile ]
+    then
+        echo $wsfile
+    else
+        echo 'No file matching: ' $wsfile
+        return 1
+    fi
 }
 
 i3-wsj-diff ()
 {
-    i3-save-tree | diff -w --color `i3-wsj-file` -
+    local WSNUM
+    if [ -z "$1" ]; then WSNUM=$(i3-ws-num); else WSNUM=$1; fi
+    local WSFILE
+    WSFILE=`i3-wsj-file $WSNUM`
+    if [ "$?" == 0 ]
+    then
+        i3-save-tree | diff -w --color $WSFILE -
+    else
+        echo $WSFILE
+    fi
+    
 }
 
