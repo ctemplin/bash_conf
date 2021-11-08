7789cbba8fc5d21613405a469f1a35fdf84da688
Templin, Chris <ctemplin@gmail.com>
2021-11-08 09:59:01 -0500

    Functions to warp mouse. One to NW corner, one to center of focused window.

--- .bashrc.d/pointer.sh
+++ .bashrc.d/pointer.sh
@@ -5,3 +5,24 @@ get_display_dimensions ()
   # from https://superuser.com/questions/418699/get-display-resolution-from-the-command-line-for-linux-desktop
   read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}')
 }
+
+warp_pointer_zero ()
+{
+  wmp -a 0 0
+}
+
+warp_pointer_con_center ()
+{
+  source ~/.bashrc.d/i3-current-workspace.sh
+  local WIN_ID XINFO
+  WIN_ID="$(i3-focused-node | jq .window)"
+  XINFO="$(xwininfo -id "${WIN_ID}")"
+  WIN_X=$(/usr/bin/grep -Po "^\s*Absolute\supper-left\sX\:\s*\K(\w*).*$"<<<"${XINFO}")
+  WIN_Y=$(/usr/bin/grep -Po "^\s*Absolute\supper-left\sY\:\s*\K(\w*.*$)"<<<"${XINFO}")
+  WIN_W=$(/usr/bin/grep -Po "^\s*Width\:\s*\K(\w*).*$"<<<"${XINFO}")
+  WIN_H=$(/usr/bin/grep -Po "^\s*Height\:\s*\K(\w*).*$"<<<"${XINFO}")
+  wmp -a $(( WIN_X + WIN_W / 2 )) $(( WIN_Y + WIN_H / 2 )) 
+}
+
+if [[ $1 == "zero" ]]; then warp_pointer_zero; fi
+if [[ $1 == "con_center" ]]; then warp_pointer_con_center; fi
