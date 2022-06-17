73a80eff31d847fb4a4ec3963907095eb0cec38c
Chris Templin <ctemplin@gmail.com>
2022-06-17 11:07:41 -0400

    Checking response code before saving WTTR response.

--- .bashrc.d/wttr_fetch.sh
+++ .bashrc.d/wttr_fetch.sh
@@ -2,5 +2,8 @@
 wttr_fetch ()
 {
    # final echo is newline for stdout readability
-  ( curl --fail -s wttr.in/?format="%c+%t+%p+%w" | tee ${HOME}/tmp/WTTR_RESP_BODY.txt ) && echo;
+   local RESP_CODE=$(curl -L wttr.in/?format="%c+%t+%p+%w" --write-out '%{http_code}' --output ${HOME}/tmp/WTTR_RESP_BODY.txt.tmp )
+   if (( $RESP_CODE == 200 )); then
+    mv ${HOME}/tmp/WTTR_RESP_BODY.txt.tmp ${HOME}/tmp/WTTR_RESP_BODY.txt
+   fi
 }
