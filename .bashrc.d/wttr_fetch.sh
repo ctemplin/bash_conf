357fa38affc22230362407ba34c0e18360bb4d4d
Chris Templin <ctemplin@gmail.com>
2022-07-30 10:33:47 -0400

    Adding simple regex sanity check to WTTR response

--- .bashrc.d/wttr_fetch.sh
+++ .bashrc.d/wttr_fetch.sh
@@ -3,7 +3,8 @@ wttr_fetch ()
 {
    # final echo is newline for stdout readability
    local RESP_CODE=$(curl -L wttr.in/?format="%c+%t+%p+%w" --write-out '%{http_code}' --output ${HOME}/tmp/WTTR_RESP_BODY.txt.tmp )
-   if (( $RESP_CODE == 200 )); then
+   local TMP_RESP=$(cat ${HOME}/tmp/WTTR_RESP_BODY.txt.tmp)
+   if [[ $RESP_CODE == 200  && $TMP_RESP =~ ^.*mph$ ]]; then
     mv ${HOME}/tmp/WTTR_RESP_BODY.txt.tmp ${HOME}/tmp/WTTR_RESP_BODY.txt
    fi
 }
