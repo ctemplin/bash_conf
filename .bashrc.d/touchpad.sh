3e9e4c0d32b7850840bdec420df3be55820b5e40
Chris Templin <ctemplin@gmail.com>
2023-11-16 12:18:53 -0600

    Add shell function to toggle touchpad tapping

--- .bashrc.d/touchpad.sh
+++ .bashrc.d/touchpad.sh
@@ -19,4 +19,25 @@ toggle_touchpad_enabled ()
 
 }
 
-if [[ $1 == "toggle" ]]; then toggle_touchpad_enabled; fi
+toggle_tapping_enabled ()
+{
+  local DEVICE_NAME 
+  DEVICE_NAME='SynPS/2 Synaptics TouchPad'
+
+  local IS_ENABLED
+  IS_ENABLED=$(xinput list-props 12 | grep -Po  '.*Tapping Enabled\s*\(\d*\):\s*\K(.*)')
+
+  if [[ $IS_ENABLED == "0" ]];
+    then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "1" 
+    notify-send -e -t 500 "Tapping" "ENABLED";
+    fi
+  if [[ $IS_ENABLED == "1" ]];
+    then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "0"
+    notify-send -e -t 500 "Tapping" "DISABLED";
+    fi
+
+}
+
+if [[ $1 == "toggle" ]]; then toggle_touchpad_enabled;
+elif [[ $1 == "tapping" ]]; then toggle_tapping_enabled;
+fi
