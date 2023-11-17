1436223991feccb22c92e9e5c5ff41f3d9d0b05b
Chris Templin <ctemplin@gmail.com>
2023-11-17 09:20:52 -0600

    Improve tapping-enable toggle Switching scroll methods too

--- .bashrc.d/touchpad.sh
+++ .bashrc.d/touchpad.sh
@@ -19,6 +19,8 @@ toggle_touchpad_enabled ()
 
 }
 
+# Toggle between tapping enabled with two-finger scrolling and
+# tapping disabled with edge scrolling enabled
 toggle_tapping_enabled ()
 {
   local DEVICE_NAME 
@@ -28,11 +30,13 @@ toggle_tapping_enabled ()
   IS_ENABLED=$(xinput list-props 12 | grep -Po  '.*Tapping Enabled\s*\(\d*\):\s*\K(.*)')
 
   if [[ $IS_ENABLED == "0" ]];
-    then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "1" 
+    then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "1" && \
+      xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "1" "0" "0"
     notify-send -e -t 500 "Tapping" "ENABLED";
     fi
   if [[ $IS_ENABLED == "1" ]];
-    then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "0"
+    then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "0" && \
+      xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "0" "1" "0"
     notify-send -e -t 500 "Tapping" "DISABLED";
     fi
 
