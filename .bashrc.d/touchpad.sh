c2057b75ed984d600592f224b1458e3af6e7085b
Chris Templin <ctemplin@gmail.com>
2023-12-02 20:14:38 -0600

    Add icon to notification

--- .bashrc.d/touchpad.sh
+++ .bashrc.d/touchpad.sh
@@ -2,19 +2,20 @@
 
 toggle_touchpad_enabled ()
 {
-  local DEVICE_NAME 
+  local DEVICE_NAME, ICON_PATH
   DEVICE_NAME='SynPS/2 Synaptics TouchPad'
+  ICON_PATH='/usr/share/icons/Papirus/128x128/devices/input-touchpad.svg'
 
   local IS_ENABLED
   IS_ENABLED=$(xinput list-props "${DEVICE_NAME}" | grep -Po  '.*Device Enabled.*:\s*\K(.*)')
 
   if [[ $IS_ENABLED == "0" ]];
     then xinput enable  "${DEVICE_NAME}" 
-    notify-send -e -t 1500 "TouchPad" "ENABLED";
+    notify-send -e -t 1500 -i "${ICON_PATH}" "TouchPad" "ENABLED";
   fi
   if [[ $IS_ENABLED == "1" ]];
     then xinput disable "${DEVICE_NAME}"
-    notify-send -e -t 1500 "TouchPad" "DISABLED";
+    notify-send -e -t 1500 -i "${ICON_PATH}" "TouchPad" "DISABLED";
   fi
 
 }
@@ -23,8 +24,9 @@ toggle_touchpad_enabled ()
 # tapping disabled with edge scrolling enabled
 toggle_tapping_enabled ()
 {
-  local DEVICE_NAME 
+  local DEVICE_NAME, ICON_PATH
   DEVICE_NAME='SynPS/2 Synaptics TouchPad'
+  ICON_PATH='/usr/share/icons/Papirus/128x128/devices/input-touchpad.svg'
 
   local IS_ENABLED
   IS_ENABLED=$(xinput list-props "${DEVICE_NAME}" | grep -Po  '.*Tapping Enabled\s*\(\d*\):\s*\K(.*)')
@@ -32,12 +34,12 @@ toggle_tapping_enabled ()
   if [[ $IS_ENABLED == "0" ]];
     then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "1" && \
       xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "1" "0" "0"
-    notify-send -e -t 1500 "Tapping" "ENABLED";
+    notify-send -e -t 1500 -i "${ICON_PATH}" "Tapping" "ENABLED";
   fi
   if [[ $IS_ENABLED == "1" ]];
     then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "0" && \
       xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "0" "1" "0"
-    notify-send -e -t 1500 "Tapping" "DISABLED";
+    notify-send -e -t 1500 -i "${ICON_PATH}" "Tapping" "DISABLED";
   fi
 
 }
