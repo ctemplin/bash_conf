112e2426485904d01dabd52993c1dde71d4946ff
Chris Templin <ctemplin@gmail.com>
2023-12-12 16:50:21 -0600

    Toggle scroll method Breaking out toggle scroll method into its own function

--- .bashrc.d/touchpad.sh
+++ .bashrc.d/touchpad.sh
@@ -30,20 +30,34 @@ main ()
     IS_ENABLED=$(xinput list-props "${DEVICE_NAME}" | grep -Po  '.*Tapping Enabled\s*\(\d*\):\s*\K(.*)')
 
     if [[ $IS_ENABLED == "0" ]];
-      then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "1" && \
-        xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "1" "0" "0"
+      then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "1"
       notify-send -e -t 1500 -i "${ICON_PATH}" "Tapping" "ENABLED";
     fi
     if [[ $IS_ENABLED == "1" ]];
-      then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "0" && \
-        xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "0" "1" "0"
+      then xinput set-prop "${DEVICE_NAME}" "libinput Tapping Enabled" "0"
       notify-send -e -t 1500 -i "${ICON_PATH}" "Tapping" "DISABLED";
     fi
 
   }
 
+  toggle_scroll_method ()
+  {
+    local IS_ENABLED
+    IS_ENABLED=$(xinput list-props "${DEVICE_NAME}" | grep -Po  '.*Scroll Method Enabled\s*\(\d*\):\s*\K(.*)')
+
+    if [[ $IS_ENABLED == "0, 1, 0" ]];
+      then xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "1" "0" "0"
+      notify-send -e -t 1500 -i "${ICON_PATH}" "Scroll Method" "TWO-FINGER";
+    fi
+    if [[ $IS_ENABLED == "1, 0, 0" ]];
+      then xinput set-prop  "${DEVICE_NAME}" "libinput Scroll Method Enabled" "0" "1" "0"
+      notify-send -e -t 1500 -i "${ICON_PATH}" "Scroll Method" "EDGE";
+    fi    
+  }
+
   if [[ $1 == "toggle" ]]; then toggle_touchpad_enabled;
   elif [[ $1 == "tapping" ]]; then toggle_tapping_enabled;
+  elif [[ $1 == "scrolling" ]]; then toggle_scroll_method;
   fi
 }
 
