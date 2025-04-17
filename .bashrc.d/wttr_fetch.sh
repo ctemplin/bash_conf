761a504379e50ffb78c01a9863df2df6c602e481
Templin, Chris <ctemplin@gmail.com>
2025-04-17 15:27:07 -0400

    handling mph or kw/h. forcing overwrite

--- .bashrc.d/wttr_fetch.sh
+++ .bashrc.d/wttr_fetch.sh
@@ -6,7 +6,7 @@ wttr_fetch ()
    # final echo is newline for stdout readability
    RESP_CODE=$(curl -L "wttr.in/${WTTR_LOC}?u&format=%l+%c+%t+%p+%w" --write-out '%{http_code}' --output "${HOME}"/tmp/WTTR_RESP_BODY.txt.tmp )
    TMP_RESP=$(cat "${HOME}"/tmp/WTTR_RESP_BODY.txt.tmp)
-   if [[ $RESP_CODE == 200  && $TMP_RESP =~ ^.*mph$ ]]; then
-    mv "${HOME}"/tmp/WTTR_RESP_BODY.txt.tmp "${HOME}"/tmp/WTTR_RESP_BODY.txt
+   if [[ $RESP_CODE == 200  && $TMP_RESP =~ ^.*[km][mp]/?h$ ]]; then
+    mv -f "${HOME}"/tmp/WTTR_RESP_BODY.txt.tmp "${HOME}"/tmp/WTTR_RESP_BODY.txt
    fi
 }
